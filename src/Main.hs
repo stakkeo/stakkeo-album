d285c743943a31d623ffe451e9f6150faf5a1468
Sergey <sergey.bushnyak@sigrlami.eu>
2019-07-30 15:12:37 +0300

    add hpack support; attach gi gtk

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,40 @@
 module Main where
 
+import           Control.Applicative
+import           Control.Concurrent
+import           Control.Lens
+import           Control.Monad          (forM_, forever, liftM, mzero, void,
+                                         when)
+import           Data.Aeson             as A
+import           Data.Aeson.Lens
+import           Data.Aeson.TH
+import           Data.Aeson.TH          (Options (..), defaultOptions,
+                                         deriveJSON)
+import           Data.Char
+import           Data.Function          (on)
+import           Data.IORef
+import           Data.List              as L (intersperse, sortBy, (\\))
+import           Data.Map               as M
+import           Data.Monoid
+import           Data.Set               as S
+import           Data.String.Utils
+import qualified Data.Text              as T
+import           Data.Text.Lens
+import           Data.Time
+import           System.Console.GetOpt
+import           System.Directory
+import           System.Directory.Extra (listFilesRecursive, removeFile)
+import           System.Environment
+
+--------------------------------------------------------------------------------
+
+locations = ["."]
+
+--importDirectory :: Filepath -> IO [T.Text]
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "stakkeo-"
+
+  Gtk.init Nothing
+  Gtk.main
